de35415330a0fc8ad4a078dcec43986a8ca1499f
Andreas Madsack <andreas@madflex.de>
2018-06-09 20:34:58 +0200

    minimal deployment (needs ssl!)

---
+++ deployment/readme.md
@@ -0,0 +1,32 @@
+### deployment using docker
+
+1) install server with docker
+
+2) checkout this repository
+
+3) add production.py
+
+```
+# coding=utf-8
+
+import os
+from .base import *
+
+DEBUG=False
+SECRET_KEY = 'ADD SOMETHING SECRET WITH SOME CHARACTERS HERE'
+STATIC_ROOT = '/opt/data/gsapi/static/'
+MEDIA_ROOT = '/opt/data/gsapi/media/'
+
+ALLOWED_HOSTS = ['*']
+```
+
+4) create database container
+
+```
+docker run -d --restart=always --name gsapi-db postgres:10
+```
+
+5) first run of update.sh
+
+comment both ``docker rm`` lines and run ``update.sh``
+
---
+++ deployment/update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+git pull
+docker pull python:3.6
+docker build --tag=gsapi-prod .
+docker rm -f gsapi
+docker run -d --link gsapi-db:db -v `pwd`/data/:/opt/data/ -v `pwd`/gsapi/gsapi/settings/production.py:/opt/code/gsapi/gsapi/settings/production.py --restart=always --name gsapi gsapi-prod
+docker rm -f gsapi-nginx
+docker run --name gsapi-nginx --net="host" --volumes-from gsapi -v `pwd`/deployment/nginx.conf:/etc/nginx/nginx.conf --restart=always -d nginx
+
+echo "Cleaning up old docker images..."
+docker rmi $(docker images | grep "<none>" | awk '{print($3)}')
